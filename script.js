c63259ec7aa5c7e066a4a9ff0fa403d0d8847161
matetipping <matetipping@gmail.com>
2021-10-16 14:28:53 +1100

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,10 @@ window.onscroll = function() {resizeHeader()};
 
 function resizeHeader() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
-    document.getElementsByTagName("header").style.height = "124px";
-    document.getElementsByTagName("header").getElementsByTagName("img").style.height = "60px";
+    var headerElems = document.getElementsByTagName("header")[0].style.height = "124px";
+    var headerImgElems = document.getElementsByTagName("header")[0].getElementsByTagName("img").style.height = "60px";
   } else {
-    document.getElementsByTagName("header").style.height = "184px";
-    document.getElementsByTagName("header").getElementsByTagName("img").style.height = "120px";
+    document.getElementsByTagName("header")[0].style.height = "184px";
+    document.getElementsByTagName("header")[0].getElementsByTagName("img").style.height = "120px";
   }
 }
