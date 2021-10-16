6fe893daa50148b569fef9d9317c59fd0aa36a7f
matetipping <matetipping@gmail.com>
2021-10-16 15:19:02 +1100

    Update script.js

--- script.js
+++ script.js
@@ -4,8 +4,10 @@ function resizeHeader() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementsByTagName("header")[0].style.height = "124px";
     document.getElementsByTagName("header")[0].getElementsByTagName("img")[0].style.height = "60px";
+    document.getElementById("nav-hamburger").style.marginTop = "0";
   } else {
     document.getElementsByTagName("header")[0].style.height = "184px";
     document.getElementsByTagName("header")[0].getElementsByTagName("img")[0].style.height = "120px";
+    document.getElementById("nav-hamburger").style.marginTop = "32px";
   }
 }
