cb3fa69c923653f82c858e86898d22b01d3e29e2
matetipping <matetipping@gmail.com>
2021-10-16 15:34:30 +1100

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,10 @@
 window.onscroll = function() {resizeHeader()};
+window.onload = function() {setHandlers();
 
+function setHandlers() {
+  document.getElementById("nav-hamburger").onclick = function() {openNavMenu()};
+}
+                            
 function resizeHeader() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementsByTagName("header")[0].style.height = "124px";
@@ -9,5 +14,13 @@ function resizeHeader() {
     document.getElementsByTagName("header")[0].style.height = "184px";
     document.getElementsByTagName("header")[0].getElementsByTagName("img")[0].style.height = "120px";
     document.getElementById("nav-hamburger").style.marginTop = "32px";
+  }
+}
+
+function openNavMenu() {
+  if (document.getElementById("nav-hamburger").classList.contains("selected")) {
+    document.getElementById("nav-hamburger").classList.remove("selected");
+  } else {
+    document.getElementById("nav-hamburger").classList.add("selected");
   }
 }
